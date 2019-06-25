7ff3a1927716f37a58de3efb277f3ca570203e20
Feather Knee <fknee@nvidia.com>
2019-06-25 12:49:49 -0700

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import { Link, Router } from '@reach/router'
+import './app.css'
 
-const App = ({ title }) => <div>{ title }</div>
+const App = () => (
+  <div className='home-container'>
+    <h1>Header</h1>
+    <nav>
+      <Link to="/">Home</Link>{" "}
+      <Link to="dashboard">Dashboard</Link>
+    </nav>
+
+    <Router>
+      <Home path='/' />
+      <Dashboard path='/dashboard' />
+    </Router>
+  </div>
+)
+
+const Home = () => (
+  <div className='home'>
+    <h2>Welcome</h2>
+  </div>
+)
+
+const Dashboard = () => (
+  <div className='dashboard'>
+    <h2>Dashboard</h2>
+  </div>
+)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.render(
-  <App title={title} />,
+  <App />,
   document.getElementById('app')
 )
 
