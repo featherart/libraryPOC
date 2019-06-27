85f8895022f8a823a8538384c5546445598fe106
Feather Knee <fknee@nvidia.com>
2019-06-27 10:34:06 -0700

    basic routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => (
   <div>
     <Header />
     <SideNavbar />
-    <Home />
   </div>
 )
 
--- src/documentation/SideNavbar/SideNavbar.js
+++ src/documentation/SideNavbar/SideNavbar.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Link } from '@reach/router'
-import { DocumentationRouter } from '../../router'
-import style from './sideNavbar.less'
+import { Link, Router } from '@reach/router'
+import { Home } from '../Home'
+import { Modal } from '../pages/Modal'
 
 export const SideNavbar = () => (
-  <div className={style.sideNavbarContainer}>
+  <div>
     <nav>
       <Link to="/">Home</Link>{" "}
-      <Link to="/modal">Modal</Link>
+      <Link to="modal">Modal</Link>
     </nav>
+
+    <Router>
+      <Home path='/' />
+      <Modal path='/modal' />
+    </Router>
   </div>
 )
