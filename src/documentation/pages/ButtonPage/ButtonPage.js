52f07540500b55b9131d37527f133597b6bd8efd
Feather Knee <fknee@nvidia.com>
2019-07-22 14:53:17 -0700

    add live editor wrapper

--- src/documentation/pages/ButtonPage/ButtonPage.js
+++ src/documentation/pages/ButtonPage/ButtonPage.js
@@ -4,6 +4,7 @@ import { theme } from '../theme'
 import { Button } from '../../../components'
 import { PropsTable } from '../PropsTable'
 import { Description } from '../Description'
+import { LiveEditorWrapper } from '../LiveEditorWrapper'
 import {
   attributes,
   description,
@@ -13,71 +14,53 @@ import {
 import '../pageStyles.less'
 
 const code = `
-render(
-  <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridGap: '25px 25px' }}>
-    <Button primary>Primary</Button>
-    <Button primary accessible>Primary Accessible</Button>
-    <Button primary disabled>Primary Disabled</Button>
-    <Button defaultStyle>Default</Button>
-    <Button defaultStyle accessible>Default Accessible</Button>
-    <Button defaultStyle disabled>Default Disabled</Button>
-    <Button flat>Flat</Button>
-    <Button flat accessible>Flat Accessible</Button>
-    <Button flat disabled>Flat Disabled</Button>
-  </div>
-)
+<div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridGap: '25px 25px' }}>
+  <Button primary>Primary</Button>
+  <Button primary accessible>Primary Accessible</Button>
+  <Button primary disabled>Primary Disabled</Button>
+  <Button defaultStyle>Default</Button>
+  <Button defaultStyle accessible>Default Accessible</Button>
+  <Button defaultStyle disabled>Default Disabled</Button>
+  <Button flat>Flat</Button>
+  <Button flat accessible>Flat Accessible</Button>
+  <Button flat disabled>Flat Disabled</Button>
+</div>
 `.trim()
 
 const utilityCode = `
-render(
-  <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridGap: '25px 25px' }}>
-    <Button primary neutral>Primary Neutral</Button>
-    <Button primary neutral disabled>Primary Neutral</Button>
-    <Button defaultStyle neutral>Default Neutral</Button>
-    <Button defaultStyle neutral disabled>Disabled</Button>
-    <Button flat neutral>Flat Neutral</Button>
-    <Button flat disabled>Flat Disabled</Button>
-  </div>
-)
+<div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridGap: '25px 25px' }}>
+  <Button primary neutral>Primary Neutral</Button>
+  <Button primary neutral disabled>Primary Neutral</Button>
+  <Button defaultStyle neutral>Default Neutral</Button>
+  <Button defaultStyle neutral disabled>Disabled</Button>
+  <Button flat neutral>Flat Neutral</Button>
+  <Button flat disabled>Flat Disabled</Button>
+</div>
 `.trim()
 
 const dangerCode = `
-render(
-  <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridGap: '25px 25px' }}>
-    <Button primary danger>Primary Danger</Button>
-    <Button primary danger disabled>Disabled</Button>
-    <Button defaultStyle danger>Default Danger</Button>
-    <Button defaultStyle danger disabled>Disabled</Button>
-    <Button flat danger>Flat Danger</Button>
-    <Button flat danger disabled>Disabled</Button>
-  </div>
-)
+<div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridGap: '25px 25px' }}>
+  <Button primary danger>Primary Danger</Button>
+  <Button primary danger disabled>Disabled</Button>
+  <Button defaultStyle danger>Default Danger</Button>
+  <Button defaultStyle danger disabled>Disabled</Button>
+  <Button flat danger>Flat Danger</Button>
+  <Button flat danger disabled>Disabled</Button>
+</div>
 `.trim()
 
 export const ButtonPage = () => (
   <div className='pageContainer'>
     <div className='title'>Buttons</div>
     <div className='subTitle'>Brand Buttons</div>
-    <Description description={description} />
-    <LiveProvider code={code} scope={{ Button }} theme={theme} noInline>
-      <LiveEditor />
-      <LiveError />
-      <LivePreview className={'displayArea'} />
-    </LiveProvider>
+      <Description description={description} />
+      <LiveEditorWrapper component={{Button}} code={code} />
     <div className='subTitle'>Utility Buttons</div>
-    <Description description={utilityDescription} />
-    <LiveProvider code={utilityCode} scope={{ Button }} theme={theme} noInline>
-      <LiveEditor />
-      <LiveError />
-      <LivePreview className={'displayArea'} />
-    </LiveProvider>
+      <Description description={utilityDescription} />
+      <LiveEditorWrapper component={{Button}} code={utilityCode} />
     <div className='subTitle'>Danger Buttons</div>
-    <Description description={dangerDescription} />
-    <LiveProvider code={dangerCode} scope={{ Button }} theme={theme} noInline>
-      <LiveEditor />
-      <LiveError />
-      <LivePreview className={'displayArea'} />
-    </LiveProvider>
+      <Description description={dangerDescription} />
+      <LiveEditorWrapper component={{Button}} code={dangerCode} />
     <PropsTable attributes={attributes} />
   </div>
 )
---
+++ src/documentation/pages/LiveEditorWrapper/LiveEditorWrapper.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { theme } from '../theme'
+
+export const LiveEditorWrapper = ({ component, code, inLine, dark }) => {
+  // this causes invariant violation, but why??
+  //const [showCode, toggleShowCode] = useState(true)
+  return (
+    <LiveProvider code={code} scope={component} theme={theme} noInline={inLine}>
+      <LivePreview className={'displayArea'} />
+      <LiveEditor />
+      <LiveError />
+    </LiveProvider>
+  )
+}
