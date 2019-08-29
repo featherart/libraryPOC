0614073aab8aa3056d8a0c42ccc5afd0b48cd7ad
Feather Knee <fknee@nvidia.com>
2019-08-29 13:22:00 -0700

    add rest of progress colors and refactor to use enum rather than boolean for colors

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,74 +1,32 @@
-// @flow
 import React from 'react'
 import cx from 'classnames'
 import './progress.less'
 
 import { ProgressGroup } from './ProgressGroup'
 
-// type Color = 'green' | 'teal' | 'blue' | 'orange' | 'red' | 'purple'
-// type ProgressT = number[]
-// type Props = {
-//   progress: ProgressT,
-//   total?: number,
-//   className?: string,
-//   color?: Color,
-//   green?: boolean,
-//   teal?: boolean,
-//   blue?: boolean,
-//   orange?: boolean,
-//   red?: boolean,
-//   purple?: boolean
-// }
+const determinWidth = (progress, total) => progress / total * 100 + '%'
+const determinColorClass = (key) => (key === 0 ? 'dark' : 'light')
 
-const determinWidth = (progress, total) =>
-  progress / total * 100 + '%'
-const determinColorClass = key => (key === 0 ? 'dark' : 'light')
-
-const ProgressBar = ({ width, colorClass }) => (
-  <div className={'bar bar-' + colorClass} style={{ width: width }} />
-)
+const ProgressBar = ({ width, colorClass }) => <div className={'bar bar-' + colorClass} style={{ width: width }} />
 
 const generateBars = (progress, total) => {
-  // const items: number[] = (progress.length) ? progress : [progress]
-  return progress
-    .map((item, key) => {
-      return {
-        progress: item,
-        colorClass: determinColorClass(key),
-        width: determinWidth(item, total)
-      }
-    })
-    .map((bar, key) => (
-      <ProgressBar key={key} colorClass={bar.colorClass} width={bar.width} />
-    ))
+	return progress
+		.map((item, key) => {
+			return {
+				progress: item,
+				colorClass: determinColorClass(key),
+				width: determinWidth(item, total)
+			}
+		})
+		.map((bar, key) => <ProgressBar key={key} colorClass={bar.colorClass} width={bar.width} />)
 }
 
-export const Progress = ({
-  progress,
-  total = 100,
-  className,
-  color,
-  green,
-  teal,
-  blue,
-  orange,
-  red,
-  purple
-}) => {
-  const styleClasses = cx(
-    color,
-    { multiple: !!progress.length },
-    { green },
-    { teal },
-    { blue },
-    { orange },
-    { red },
-    { purple }
-  )
-  const classes = cx('ui', 'progress', className, styleClasses)
-  const bars = generateBars(progress, total)
-  return <div className={classes}>{bars}</div>
+export const Progress = ({ progress, total = 100, className, color }) => {
+	const styleClasses = cx(color, { multiple: !!progress.length })
+	const classes = cx('ui', 'progress', className, styleClasses)
+	const bars = generateBars(progress, total)
+	return <div className={classes}>{bars}</div>
 }
 
-// Add sub components.
+// sub components.
 Progress.Group = ProgressGroup
--- src/documentation/pages/ProgressPage/ProgressPage.js
+++ src/documentation/pages/ProgressPage/ProgressPage.js
@@ -11,13 +11,48 @@ const code = `
 <Progress progress={[49]} />
 `.trim()
 
+const customCode = `
+<Progress progress={[50, 80]} total={150} />
+`.trim()
+
+const tealCode = `
+<Progress color={'teal'} progress={[20, 50]} total={175} />
+`.trim()
+
+const blueCode = `
+<Progress color={'blue'} progress={[43, 65]} />
+`.trim()
+
+const purpleCode = `
+<Progress color='purple' progress={[20, 65]} />
+`.trim()
+
+const orangeCode = `
+<Progress color='orange' progress={[20, 60]} total={175} />
+`.trim()
+
+const redCode = `
+<Progress color='red' progress={[20, 60]} total={140} />
+`.trim()
 
 export const ProgressPage = () => (
-	<div className={'pageContainer'}>
-		<div className={'title'}>Progress</div>
+	<div className='pageContainer'>
+		<div className='title'>Progress</div>
 		<Description description={description} />
 		<div className='subTitle'>Default (Green)</div>
-		<LiveEditorWrapper component={{ Progress }} code={code} />
+		<LiveEditorWrapper small component={{ Progress }} code={code} />
+		<div className='subTitle'>Custom Total</div>
+		<LiveEditorWrapper small component={{ Progress }} code={customCode} />
+		<div className='subTitle'>Teal Progress</div>
+		<LiveEditorWrapper small component={{ Progress }} code={tealCode} />
+		<div className='subTitle'>Blue Progress</div>
+		<LiveEditorWrapper small component={{ Progress }} code={blueCode} />
+		<div className='subTitle'>Purple Progress</div>
+		<LiveEditorWrapper small component={{ Progress }} code={purpleCode} />
+		<div className='subTitle'>Orange Progress</div>
+		<LiveEditorWrapper small component={{ Progress }} code={orangeCode} />
+		<div className='subTitle'>Red Progress</div>
+		<LiveEditorWrapper small component={{ Progress }} code={redCode} />
 		<PropsTable attributes={attributes} />
 	</div>
 )
